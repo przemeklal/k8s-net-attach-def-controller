d0fb8ccc94773236cd730cf7a2075502b070f388
Przemyslaw Lal <przemyslawx.lal@intel.com>
2019-03-21 16:46:39 +0000

    add skeleton code for service events hooks

---
+++ cmd/k8s-net-attach-def-controller/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	clientset "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/clientset/versioned"
+	sharedInformers "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/informers/externalversions"
+
+	"github.com/K8sNetworkPlumbingWG/k8s-net-attach-def-controller/pkg/controller"
+)
+
+var (
+	master     string
+	kubeconfig string
+
+	// defines default resync period between k8s API server and controller
+	syncPeriod = time.Second * 5
+)
+
+func main() {
+	flag.StringVar(&master, "master", "",
+		"The address of the Kubernetes API server. Overrides any value in kubeconfig. Required if out-of-cluster.")
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Required if out-of-cluster.")
+	flag.Parse()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
+	if err != nil {
+		log.Fatalf("error building kubeconfig: %s", err.Error())
+	}
+
+	k8sClientSet, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Fatalf("error creating kubernetes clientset: %s", err.Error())
+	}
+
+	netAttachDefClientSet, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		log.Fatalf("error creating net-attach-def clientset: %s", err.Error())
+	}
+
+	netAttachDefInformerFactory := sharedInformers.NewSharedInformerFactory(netAttachDefClientSet, syncPeriod)
+	k8sInformerFactory := informers.NewSharedInformerFactory(k8sClientSet, syncPeriod)
+
+	networkController := controller.NewNetworkController(
+		k8sClientSet,
+		netAttachDefClientSet,
+		netAttachDefInformerFactory.K8sCniCncfIo().V1().NetworkAttachmentDefinitions(),
+		k8sInformerFactory.Core().V1().Services(),
+		k8sInformerFactory.Core().V1().Pods(),
+		k8sInformerFactory.Core().V1().Endpoints(),
+	)
+
+	stopChan := make(chan struct{})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		close(stopChan)
+		<-c
+		os.Exit(1)
+	}()
+
+	netAttachDefInformerFactory.Start(stopChan)
+	k8sInformerFactory.Start(stopChan)
+	networkController.Start(stopChan)
+}
